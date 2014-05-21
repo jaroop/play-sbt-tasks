c9439f07caca095a7224059621bef9e3bc22b763
Michael Zajac <mzajac@jaroop.com>
2014-05-20 21:21:50 -0400

    Added scaladoc

--- src/main/scala/Task.scala
+++ src/main/scala/Task.scala
@@ -5,6 +5,9 @@ import Keys._
 import java.net.URLClassLoader
 import play.core.StaticApplication
 
+/** An abstract `Runnable` class` that must implement a `run` function that returns `Unit`.
+* @param arguments Arguments that can be passed to the task.
+*/
 abstract class Task(arguments: String) extends Runnable {
 
   val application = new StaticApplication(new java.io.File("."))
@@ -13,6 +16,12 @@ abstract class Task(arguments: String) extends Runnable {
 
 object Task {
 
+	/** Creates a runnable play sbt task that can be added to application settings in Build.scala or build.sbt
+	* @param name The name used to invoke the task via sbt.
+	* @param className The fully qualified package/className of the `Runnable` task (e.g. com.jaroop.tasks.MyTask).
+	* @param description A short description of the task shown when running `sbt tasks`.
+	* @return The sbt task to include in sbt settings for an application.
+	*/
 	def register(name: String, className: String, description: String):Def.Setting[sbt.InputTask[Unit]] = {
     
 	    InputKey[Unit](name, description) <<= inputTask {
