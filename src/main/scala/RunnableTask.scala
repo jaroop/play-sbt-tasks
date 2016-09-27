06bb4d1c6cecf33ee057464a669d8b5f1075f903
COtlowski <c.otlows@gmail.com>
2016-09-27 14:50:18 -0400

    Added execute method with a Application param

--- src/main/scala/RunnableTask.scala
+++ src/main/scala/RunnableTask.scala
@@ -1,6 +1,6 @@
 package com.jaroop.play.sbt
 
-import play.api.{ApplicationLoader, Environment, Mode, Play}
+import play.api.{ApplicationLoader, Environment, Mode, Play, Application}
 
 /** An abstract `Runnable` class that must implement an `execute` function that returns `Unit`.
  *  @param arguments Arguments that can be passed to the task.
@@ -10,13 +10,15 @@ abstract class RunnableTask(arguments: String) extends Runnable {
     /** The task method to run inside of an ad-hoc Play application. */
     def execute(): Unit
 
+    def execute(app: Application): Unit
+
     /** Run the task within an ad-hoc Play application, and shut it down when finished. */
     def run(): Unit = {
         val env = Environment(new java.io.File("."), this.getClass.getClassLoader, Mode.Dev)
         val context = ApplicationLoader.createContext(env)
         val loader = ApplicationLoader(context)
         val app = loader.load(context)
-        try { this.execute() } finally { Play.stop(app) }
+        try { this.execute(app) } finally { Play.stop(app) }
     }
 
 }
